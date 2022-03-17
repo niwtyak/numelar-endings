1a75aa59e60a5d2bad2dc9a140a027983f3932d2
niwtyak <alex.pakschin@gmail.com>
2022-03-17 15:21:05 +0300

    fix

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,9 +3,8 @@ import kotlin.math.log10
 fun numeral(likes:Int):String{
     return when{
         likes == 0 || log10(likes.toDouble()) % 1 == 0.0 && likes != 10 && likes != 100-> "человек"
-        likes % 10.0 == 1.0 -> "человеку"
-        likes % 10.0 in 2.0 .. 9.0 || likes % 10.0 == 0.0 -> "людям"
-        else -> ""
+        likes % 10.0 == 1.0 && likes % 100.0 != 11.0 -> "человеку"
+        else -> "людям"
     }
 }
 
@@ -14,7 +13,7 @@ fun main(){
     val amount1 = 76121
     val amount2 = 174824
     val amount3 = 0
-    val amount4 = 100
+    val amount4 = 1111
 
     println("Понравилось $amount ${numeral(amount)}")
     println("Понравилось $amount1 ${numeral(amount1)}")
